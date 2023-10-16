7c1876cb08132f61f7de44d75bfb353fc9ae6199
Simon Nilsson <simon.nilsson.10@outlook.com>
2023-10-16 18:33:40 +0200

    Add project files.

---
+++ Labb 1, Grunderna i OOP/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Labb_1__Grunderna_i_OOP //Simon Nilsson SUT23
+{
+    class Circle
+    {
+        int Radius;
+        float pi = 3.141f;
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+        public double getArea()
+        {
+            return (Radius * Radius * pi);
+        }        
+        public double getCircum()
+        {
+            return (2 * pi * Radius); 
+        }
+        public double getVolume()
+        {
+            return ((4 * pi * Radius * Radius * Radius) / 3); //Mathematic to calculate the volume och the circle. 
+        }        
+    }
+    class Triangle
+    {
+        public double buttom { get; set; }
+        public double height { get; set; }
+        public double getTriangleArea()
+        {
+            return ((buttom * height) / 2);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            Console.WriteLine($"Cirkel nr 1:\nArea: {circle1.getArea()}\nOmkrets: {circle1.getCircum()}\nVolym: {circle1.getVolume()}\n");
+            Console.WriteLine($"Cirkel nr 2:\nArea: {circle2.getArea()}\nOmkrets: {circle2.getCircum()}\nVolym: {circle2.getVolume()}\n");
+
+            Triangle triangle = new Triangle();
+            Console.Write("Skriv in basen på din triangel: ");
+            triangle.buttom = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Skriv in höjden på din triangel: ");
+            triangle.height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"\nArean på din triangel är: {triangle.getTriangleArea()}");
+            
+        }
+    }
+}
